4c67e2b98c8e58665a6fdc606defebfddf8dfb5b
smilexuhc <smile.xuhc@gmail.com>
2020-06-11 09:55:53 +0800

    add default hparams parse component for convenience

--- parse_args.py
+++ parse_args.py
@@ -1,14 +1,45 @@
 import argparse
+import yaml
+from easydict import EasyDict
 
 
-def parse_args():
+def parse_config(config_path):
+    with open(config_path) as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    return config
+
+
+# def parse_args():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--dataset', type=str, default='flickr')
+#     parser.add_argument('--train_sample', type=int, default=1, choices=[0, 1])
+#     parser.add_argument('--eval_sample', type=int, default=0, choices=[0, 1])
+#     parser.add_argument('--loss_norm', type=int, default=1, choices=[0, 1])
+#     parser.add_argument('--epochs', type=int, default=500)
+#     parser.add_argument('--batch_size', type=int, default=6000)
+#     parser.add_argument('--sampler', type=str, default='rw',
+#                         choices=['rw-my', 'rw', 'ns', 'node-my', 'edge', 'node', 'cluster'])
+#     parser.add_argument('--gcn_type', type=str, default='sage', choices=['sage', 'gat'])
+#     parser.add_argument('--use_gpu', type=int, default=1, choices=[0, 1])
+#     parser.add_argument('--save_log', type=int, default=1, choices=[0, 1])
+#     parser.add_argument('--save_summary', type=int, default=1, choices=[0, 1])
+#     parser.add_argument('--log_interval', type=int, default=10)
+#
+#     args = parser.parse_args()
+#     if args.train_sample == 0:
+#         args.loss_norm = 0
+#
+#     return args
+
+
+def parse_args(config_path):
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str, default='flickr')
     parser.add_argument('--train_sample', type=int, default=1, choices=[0, 1])
     parser.add_argument('--eval_sample', type=int, default=0, choices=[0, 1])
     parser.add_argument('--loss_norm', type=int, default=1, choices=[0, 1])
     parser.add_argument('--epochs', type=int, default=500)
-    parser.add_argument('--batch_size', type=int, default=6000)
+    parser.add_argument('--batch_size', type=int)
     parser.add_argument('--sampler', type=str, default='rw',
                         choices=['rw-my', 'rw', 'ns', 'node-my', 'edge', 'node', 'cluster'])
     parser.add_argument('--gcn_type', type=str, default='sage', choices=['sage', 'gat'])
@@ -20,8 +51,15 @@ def parse_args():
     args = parser.parse_args()
     if args.train_sample == 0:
         args.loss_norm = 0
+    args = vars(args)
 
-    return args
+    config = parse_config(config_path)
+    config = config[args['dataset']]
+    config = EasyDict(config)
+    for k, v in args.items():
+        if v is not None:
+            config[k] = v
+    return config
 
 
 def get_log_name(args, prefix='test', use_args=None):
@@ -30,3 +68,4 @@ def get_log_name(args, prefix='test', use_args=None):
     args = vars(args)
     log_name = prefix + '-' + '-'.join([arg + '=' + str(args[arg]) for arg in use_args])
     return log_name
+
